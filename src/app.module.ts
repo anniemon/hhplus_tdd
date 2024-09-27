8b3efdd4d3db6fc3e22815d676c592af388f456b
anniemon <seojung19@gmail.com>
2024-09-28 02:47:47 +0900

    misc: import db module in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PointModule } from './point/point.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PointModule],
+  imports: [PointModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
